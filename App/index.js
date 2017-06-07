53a612b992b61763dd00edaa25137290a2808e8c
Jeff Chapman <jeffcchapman@gmail.com>
2017-06-06 20:27:00 -0600

    set up initial react app from scratch

---
+++ App/index.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from 
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello React</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
